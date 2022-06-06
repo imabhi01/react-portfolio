5fccb244afcf7f7028043daa82b2d5786fa0aa60
imabhi01 <iamdev.abhishek@gmail.com>
2022-06-06 14:17:04 +0545

    react portfolio website init

---
+++ backend_sanity/schemas/brands.js
@@ -0,0 +1,20 @@
+export default{
+    name: 'brand',
+    title: 'Brands',
+    type: 'document',
+    fields: [
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string'
+        },
+        {
+            name: 'imgUrl',
+            title: 'ImgUrl',
+            type: 'image',
+            options: {
+                hotspot: true
+            }
+        }
+    ]
+}
---
+++ backend_sanity/schemas/schema.js
@@ -0,0 +1,28 @@
+// First, we must import the schema creator
+import createSchema from 'part:@sanity/base/schema-creator'
+
+// Then import schema types from any plugins that might expose them
+import schemaTypes from 'all:part:@sanity/base/schema-type'
+
+import testimonials from './testimonials'
+import experiences from './experiences'
+import workExperience from './workExperience'
+import brands from './brands'
+import skills from './skills'
+import about from './about'
+import works from './works'
+import contact from './contact'
+
+// Then we give our schema to the builder and provide the result to Sanity
+export default createSchema({
+  name: 'default',
+  types: schemaTypes.concat([
+    testimonials,
+    workExperience,
+    experiences,
+    brands,
+    about,
+    skills,
+    works
+  ]),
+})
