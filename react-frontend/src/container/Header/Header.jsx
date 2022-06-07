2faedde9118176f8e4b6982c1d6a859c9c40ac38
imabhi01 <iamdev.abhishek@gmail.com>
2022-06-07 15:03:53 +0545

    header design

--- react-frontend/src/container/Header/Header.jsx
+++ react-frontend/src/container/Header/Header.jsx
@@ -33,7 +33,7 @@ const Header = () => {
           </div>
 
           <div className='tag-cmp app__flex'>
-            <p className='p-text'>Full Stack Developer</p>
+            <p className='p-text'> Developer</p>
             <p className='p-text'>Designer</p>
           </div>
         </div>
