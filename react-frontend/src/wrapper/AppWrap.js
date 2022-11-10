518d10ec2d87a51abcd093b24bd9b35dbe7ac10b
Abhishek Chaudhary <abhishek.chaudhary@8squarei.com>
2022-11-10 08:41:19 +0545

    App wrap changes

--- react-frontend/src/wrapper/AppWrap.js
+++ react-frontend/src/wrapper/AppWrap.js
@@ -5,20 +5,15 @@ const AppWrap = (Component, idName, classNames) => function HOC(){
 
   return (
     <div id={idName} className={`app__container ${classNames}`}>
-      
       <SocialMedia/>
-
       <div className="app__wrapper app__flex">
         <Component />
-
         <div className='copyright'>
           <p className='p-text'>@2022</p>
           <p className='p-text'>All rights reserved!</p>
         </div>
       </div>
-
       <NavigationDots active={idName} />
-      
     </div>
   )
 }
