3246ee41855e123b77f1d0dcdb15b2009f8acb6d
Abhishek Chaudhary <abhishek.chaudhary@8squarei.com>
2022-11-10 23:08:27 +0545

    app wrap

--- react-frontend/src/wrapper/AppWrap.js
+++ react-frontend/src/wrapper/AppWrap.js
@@ -2,7 +2,6 @@ import React from 'react'
 import {NavigationDots, SocialMedia} from '../components'
 
 const AppWrap = (Component, idName, classNames) => function HOC(){
-
   return (
     <div id={idName} className={`app__container ${classNames}`}>
       <SocialMedia/>
