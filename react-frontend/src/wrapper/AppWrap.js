6da007a87b52142ac614547f4cd6a3bd3e78bde5
Abhishek Chaudhary <abhishek.chaudhary@8squarei.com>
2022-11-11 16:32:58 +0545

    Current year fixes

--- react-frontend/src/wrapper/AppWrap.js
+++ react-frontend/src/wrapper/AppWrap.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import { useState } from 'react';
 import {NavigationDots, SocialMedia} from '../components'
 
 const AppWrap = (Component, idName, classNames) => function HOC(){
+  const current = new Date();
   return (
     <div id={idName} className={`app__container ${classNames}`}>
       <SocialMedia/>
       <div className="app__wrapper app__flex">
         <Component />
         <div className='copyright'>
-          <p className='p-text'>@2022</p>
+          <p className='p-text'>@{current.getFullYear()}</p>
           <p className='p-text'>All rights reserved!</p>
         </div>
       </div>
