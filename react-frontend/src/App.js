8a4e05a5263bce22d780b07ef71688437ca96e4e
Abhishek Chaudhary <abhishek.chaudhary@8squarei.com>
2022-11-24 15:35:24 +0545

    app.js google analytics

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -6,14 +6,11 @@ import './App.scss'
 import ReactGa from 'react-ga'
 
 const App = () => {
-
   useEffect(() => {
     ReactGa.initialize('UA-163282676-2')
     // to report page view 
     ReactGa.pageview('/')
   }, [])
-  
-
   return (
     <div className="app">
       <Navbar />
